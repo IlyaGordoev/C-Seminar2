c7477a94eb73556ea686cbd42be8438c34ed0d41
Ilya <gordoev@yandex.ru>
2023-04-23 12:26:24 +0300

    Add seminar 2

---
+++ task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number  = new Random().Next(99, 1000);
+Console.WriteLine($"Случайное число: {number}");
+int digit1 = number/100;
+int digit3 = number%10;
+{
+    int result = digit1 * 10 + digit3;
+    Console.WriteLine(result);
+    // Console.Write($"{digit1}{digit3}");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == Math.Pow(numberB,2) || numberB == Math.Pow(numberA,2)) // Math.Pow(numberB,2) numberB * numberB
+{
+    Console.WriteLine($"Да");
+}
+else
+{
+    Console.WriteLine($"Нет");
+}
